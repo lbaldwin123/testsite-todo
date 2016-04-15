85fd7cd074ef424981aed2738f40011cebe30980
Lonnie Baldwin <lbaldwin@grubhub.com>
2016-04-15 15:26:46 -0400

    updated handlers to handle delete

--- handlers.go
+++ handlers.go
@@ -81,26 +81,27 @@ func TodoCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoDestroy(w http.ResponseWriter, r *http.Request) {
-	var todo Todo
-	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-	if err != nil {
+	vars := mux.Vars(r)
+	var todoId int
+	var err error
+	if todoId, err = strconv.Atoi(vars["todoId"]); err != nil {
 		panic(err)
 	}
-	if err := r.Body.Close(); err != nil {
-		panic(err)
-	}
-	if err := json.Unmarshal(body, &todo); err != nil {
+	todo := RepoDestroyTodo(todoId)
+	if todo.Id > 0 {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-		w.WriteHeader(422) // unprocessable entity
-		if err := json.NewEncoder(w).Encode(err); err != nil {
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(todo); err != nil {
 			panic(err)
 		}
+		return
 	}
 
-	t := RepoDestroyTodo(todo)
+	// If we didn't find it, 404
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(t); err != nil {
+	w.WriteHeader(http.StatusNotFound)
+	if err := json.NewEncoder(w).Encode(jsonErr{Code: http.StatusNotFound, Text: "Not Found"}); err != nil {
 		panic(err)
 	}
+
 }
